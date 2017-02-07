1418d484e247f2cf7893c6f95a8d3d5ffcc2dac0
Tory Wheelwright <twheelwright@gmail.com>
2017-02-06 23:15:09 -0500

    Implement insertion sort

--- src/algo/2.1-selection-less.js
+++
@@ -1,7 +0,0 @@
-// Java:private static boolean less(Comparable v, Comparable w) {
-less(v, w) {
-// Java:  return v.compareTo(w) < 0;
-  vm.setResult(that.args["v"] < that.args["w"]);
-  that.nextLine = undefined;
-// Java:}
-}
