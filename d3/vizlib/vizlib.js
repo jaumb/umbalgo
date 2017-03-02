01318cfab33f9ec4d254a8f2e21f1675bdd0db79
Jason Curran <curran.jm@gmail.com>
2017-03-02 17:27:32 -0500

    Added algorithm names.

--- d3/vizlib/vizlib.js
+++ d3/vizlib/vizlib.js
@@ -9,12 +9,72 @@ var vizlib = (function() {
   /****************************************************************************
    *  private methods
    ****************************************************************************/
+  // CHAPTER 1
+  // Knuth.java
+  // BinarySearch.java
+  // LinkedBag.java
+  // ResizingArrrayBag.java
+  // LinkedQueue.java
+  // ResizingArrrayQueue.java
+  // LinkedStack.java
+  // ResizingArrrayStack.java
+  // QuickFindUF.java
+  // QuickUnionUF.java
+  // WeightedQuickUnionUF.java
+  //
+  // CHAPTER 2
+  // Selection.java
+  // Insertion.java
+  // Shell.java
+  // Merge.java
+  // Quick.java
+  // Quick3way.java
+  // Heap.java
+  // MaxPQ.java
+  // MinPQ.java
+  //
+  // CHAPTER 3
+  // SequentialSearchST.java
+  // BinarySearchST.java
+  // BST.java
+  // RedBlackBST.java
+  // SeparateChainingHashST.java
+  //
+  // CHAPTER 4
+  // Graph.java
+  // DepthFirstPaths.java
+  // BreadthFirstPaths.java
+  // CC.java
+  // Digraph.java
+  // DirectedCycle.java
+  // DepthFirstOrder.java
+  // Topological.java
+  // KosarajuSharirSCC.java
+  // EdgeWeightedGraph.java
+  // KruskalMST.java
+  // EdgeWeightedDigraph.java
+  // DijkstraSP.java
+  // AcyclicSP.java
+  //
+  // CHAPTER 5
+  // LSD.java
+  // MSD.java
+  // TrieST.java
+  // KMP.java
+  // NFA.java
+  // RunLength.java
+  // Huffman.java
+
+
+
 
 
   /****************************************************************************
    *  public methods
    ****************************************************************************/
+   var get = function(algorithm) {
 
+   }
 
   /****************************************************************************
    *  return public methods
