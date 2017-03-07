73c66a9796e9a86fa26a8ab573fbe22824380d07
Jason Curran <curran.jm@gmail.com>
2017-03-07 02:08:26 -0500

    selection updates for testing

--- d3/vizlib/algorithms/selection.js
+++ d3/vizlib/algorithms/selection.js
@@ -3,16 +3,84 @@
  * Selection Sort visualization object.
  * @param {Object} input - Object containing data and canvas constraints.
  */
-var selection = (function(input) {
+var selection = (function(elems, bounding_box) {
   //////////////////////////////////////////////////////////////////////////////
   // private variables
   //////////////////////////////////////////////////////////////////////////////
   var min = element_factory.rect();
-  var array = array_factory.(input.data, input.size);
+  var array = array_factory.get_array(elems, bounding_box);
 
   //////////////////////////////////////////////////////////////////////////////
   // private methods
   //////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * Set the value of the min element's label.
+   * @param {string} newVal - new
+   */
+  var _setMinLabel = function(new_Val) {
+    min.label.val = new_Val;
+  }
+
+  /**
+   * Set the coordinates of the min element.
+   * @param {object} array_element - Array element to align min with.
+   */
+  var _setMinPos = function(array_element) {
+    min.pos.x = array_element.pos.x;
+  }
+
+  /**
+   * Set the fill color of the min element.
+   * @param {string} new_color - New fill color of min element.
+   */
+  var _setMinFill = function(new_color) {
+    min.fill = new_color;
+  }
+
+  /**
+   * Emphasize (thicken border of) a rect element.
+   * @param {number[]} indices - Array of indices to emphasize.
+   * @param {string} color - Color for emphasis border.
+   */
+  var _emphasize = function(indices, color) {
+    array.emphasize(indices, color);
+  }
+
+  /**
+   * Demphasize (remove thick border around) a rect element.
+   * @param {number[]} indices - Array of indices to deemphasize.
+   */
+  var _deemphasize = function(indices) {
+    array.deemphasize(indices);
+  }
+
+  /**
+   * Use array's swap function to swap two elements in the array.
+   * @param {number} index1 - Index of first element to swap.
+   * @param {number} index2 - Index of second element to swap.
+   */
+  var _swap = function(index1, index2) {
+    array.swap(index1, index2);
+  }
+
+  /**
+   * Change the fill color of elements at specified indices.
+   * @param {number[]} elements - Array of element indices.
+   * @param {string} new_color - New fill color for specified indices.
+   */
+  var _setFill = function(elements, new_color) {
+    array.setFill(elements, new_color);
+  }
+
+  /**
+   * Set val of label property for elements at specified indices.
+   * @param {number[]} indices - Indices of elements to get new val.
+   * @param {string} val - Value to give to elements at specified indexes.
+   */
+  var _setLabels = function(indices, val) {
+    array.setLabels(elements, val);
+  }
 
 
   //////////////////////////////////////////////////////////////////////////////
@@ -22,7 +90,7 @@ var selection = (function(input) {
 
   //element array getters///////////////////////////////////////////////////////
   var getRects = function() {
-    return vizlib.getRects(array, min, emphasis);
+    return vizlib.getRects(array, min);
   }
 
   var getCircles = function() {
@@ -38,74 +106,9 @@ var selection = (function(input) {
   }
   //end of element array getters////////////////////////////////////////////////
 
-  /**
-   * Set the value of the min element's label.
-   * @param {string} newVal - new
-   */
-  var setMinLabel = function(new_Val) {
-    min.label.val = new_Val;
+  var setFill = function(indices, color) {
+    vizupdate.addOperation(_setFill(indices, color));
   }
-
-  /**
-   * Set the coordinates of the min element.
-   * @param {object} array_element - Array element to align min with.
-   */
-  var setMinPos = function(array_element) {
-    min.pos.x = array_element.pos.x;
-  }
-
-  /**
-   * Set the fill color of the min element.
-   * @param {string} new_color - New fill color of min element.
-   */
-  var setMinFill = function(new_color) {
-    min.fill = new_color;
-  }
-
-  /**
-   * Emphasize (thicken border of) a rect element.
-   * @param {number[]} indices - Array of indices to emphasize.
-   * @param {string} color - Color for emphasis border.
-   */
-  var emphasize = function(indices, color) {
-    array.emphasize(indices, color);
-  }
-
-  /**
-   * Demphasize (remove thick border around) a rect element.
-   * @param {number[]} indices - Array of indices to deemphasize.
-   */
-  var deemphasize = function(indices) {
-    array.deemphasize(indices);
-  }
-
-  /**
-   * Use array's swap function to swap two elements in the array.
-   * @param {number} index1 - Index of first element to swap.
-   * @param {number} index2 - Index of second element to swap.
-   */
-  var swap = function(index1, index2) {
-    array.swap(index1, index2);
-  }
-
-  /**
-   * Change the fill color of elements at specified indices.
-   * @param {number[]} elements - Array of element indices.
-   * @param {string} new_color - New fill color for specified indices.
-   */
-  var setFill = function(elements, new_color) {
-    array.setFill(elements, new_color);
-  }
-
-  /**
-   * Set val of label property for elements at specified indices.
-   * @param {number[]} indices - Indices of elements to get new val.
-   * @param {string} val - Value to give to elements at specified indexes.
-   */
-  setLabels = function(indices, val) {
-    array.setLabels(elements, val);
-  }
-
 
 
   //////////////////////////////////////////////////////////////////////////////
@@ -115,6 +118,14 @@ var selection = (function(input) {
     getRects:getRects,
     getCircles:getCircles,
     getLines:getLines,
-    getText:getText
+    getText:getText,
+    // setMinLabel:setMinLabel,
+    // setMinPos:setMinPos,
+    // setMinFill:setMinFill,
+    // emphasize:emphasize,
+    // deemphasize:deemphasize,
+    // swap:swap,
+    setFill:setFill,
+    // setLabels:setLabels
   }
 });
