b1f1fd4521c590c99100e990a1f6292386520435
Tory Wheelwright <twheelwright@gmail.com>
2017-04-15 13:14:45 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -v
+
+WD=$(pwd)
+PARSE=${WD}/tools/parser.py
+IN=${WD}/src/algo
+OUT=/var/www/html/
+
+rm -rf docs
+mkdir -p ${OUT}/js/algo
+pushd .
+cd ${IN}
+for f in *.js
+do
+  ${PARSE} $f ${OUT}/js/algo/$f
+done
+popd
+
+pushd .
+cd src
+../node_modules/.bin/babel -d ${OUT}/js/libs libs
+../node_modules/.bin/babel -d ${OUT}/js/content content
+popd
+
+cp src/html/*.html ${OUT}
+mkdir -p ${OUT}/css
+cp src/css/*.css ${OUT}/css
+cp -r d3/vizlib ${OUT}/js/libs
