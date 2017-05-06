c67ae08807147f3f3f61719d48a291800b66dbec
nikhilsidhwani <nikhil.sidhwani001@umb.edu>
2017-05-06 02:59:16 -0400

    Update about.js

--- src/content/about.js
+++ src/content/about.js
@@ -1,82 +1,88 @@
 
 document.getElementById("container").innerHTML += `
 <div class="about-us">
-<div class="info">
-<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-</div>
+  <div class="info">
+    
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p>
 
-<div class="row">
-
-<div class = "col-sm-6 col-md-2 col-md-offset-1">
-  <div class="profile-card">
-   <a href="#"><span></span></a>
-    <div class="avatar"><img src=""/></div>
-    <div class="bottom-section">
-    <a href="#"><span></span></a>
-      <h3>name</h3>
-      <p>information about each team member</p>
-    </div>
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    </p>
   </div>
-</div>
 
-<div class = "col-sm-6 col-md-2">
-  <div class="profile-card">
-   <a href="#"><span></span></a>
-    <div class="avatar"><img src=""/></div>
-    <div class="bottom-section">
-    <a href="#"><span></span></a>
-      <h3>name</h3>
-      <p>information about each team member</p>
-    </div>
-  </div>
-</div>
+  <div class="row">
 
-<div class = "col-sm-6 col-md-2">
-  <div class="profile-card">
-   <a href="#"><span></span></a>
-    <div class="avatar"><img src=""/></div>
-    <div class="bottom-section">
-    <a href="#"><span></span></a>
-      <h3>name</h3>
-      <p>information about each team member</p>
+    <div class = "col-sm-6 col-md-2 col-md-offset-1">
+      <div class="profile-card">
+        <a href="#"><span></span></a>
+        <div class="avatar"><img src=""/></div>
+        <div class="bottom-section">
+          <a href="#"><span></span></a>
+          <h3>name</h3>
+          <p>information about each team member</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
-<div class = "col-sm-6 col-md-2">
-  <div class="profile-card">
-   <a href="#"><span></span></a>
-    <div class="avatar"><img src=""/></div>
-    <div class="bottom-section">
-    <a href="#"><span></span></a>
-      <h3>name</h3>
-      <p>information about each team member</p>
+    <div class = "col-sm-6 col-md-2">
+      <div class="profile-card">
+        <a href="#"><span></span></a>
+        <div class="avatar"><img src=""/></div>
+        <div class="bottom-section">
+          <a href="#"><span></span></a>
+          <h3>name</h3>
+          <p>information about each team member</p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 
-<div class = "col-sm-6 col-md-2">
-  <div class="profile-card">
-   <a href="#"><span></span></a>
-    <div class="avatar"><img src=""/></div>
-    <div class="bottom-section">
-    <a href="#"><span></span></a>
-      <h3>name</h3>
-      <p>information about each team member</p>
+    <div class = "col-sm-6 col-md-2">
+      <div class="profile-card">
+        <a href="#"><span></span></a>
+        <div class="avatar"><img src=""/></div>
+        <div class="bottom-section">
+          <a href="#"><span></span></a>
+          <h3>name</h3>
+          <p>information about each team member</p>
+        </div>
+      </div>
     </div>
+
+    <div class = "col-sm-6 col-md-2">
+      <div class="profile-card">
+        <a href="#"><span></span></a>
+        <div class="avatar"><img src=""/></div>
+        <div class="bottom-section">
+          <a href="#"><span></span></a>
+          <h3>name</h3>
+          <p>information about each team member</p>
+        </div>
+      </div>
+    </div>
+
+    <div class = "col-sm-6 col-md-2">
+      <div class="profile-card">
+        <a href="#"><span></span></a>
+        <div class="avatar"><img src=""/></div>
+        <div class="bottom-section">
+          <a href="#"><span></span></a>
+          <h3>name</h3>
+          <p>information about each team member</p>
+        </div>
+      </div>
+    </div>
+
   </div>
-</div>
-</div>
+  
 </div>
 `;
