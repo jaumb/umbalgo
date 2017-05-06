b2803f234afb1f3c6c566f374f244cdaf8a23348
Tory Wheelwright <twheelwright@gmail.com>
2017-05-06 14:50:06 -0400

    remove test data set

--- src/content/algorithms/sorting/insertion-sort.js
+++ src/content/algorithms/sorting/insertion-sort.js
@@ -3,8 +3,7 @@ vm.loadFunc("2.1-insertion-sort.js");
 populateSelectInput({
   "Best Case": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   "Worst Case": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-  "Random": d3.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
-  "test": d3.shuffle([1, 2, 3, 4]) // TODO: REMOVE
+  "Random": d3.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 });
 
 let onInvoke = function() {
