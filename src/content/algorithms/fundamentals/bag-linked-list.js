93169851df2b84774bcec240bf9dffee6fbab72a
Tory Wheelwright <twheelwright@gmail.com>
2017-05-07 08:43:23 -0400

    add omitted bag

---
+++ src/content/algorithms/fundamentals/bag-linked-list.js
@@ -0,0 +1,22 @@
+vm.loadFunc("1.3-bag-add.js");
+
+populateSelectInput({
+  "Random": d3.shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+  "Ascending": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  "Descending": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+});
+
+let firstInvocation = true;
+
+var onInvoke = function() {
+  if (firstInvocation === true) {
+    firstInvocation = false;
+    vm.globals["first"] = null;
+    vm.viz = vizlib.get_linkedbag(vm.globals["first"], svgW, svgH);
+    vm.viz.showFirst();
+    vm.viz.updateCanvas(vm.dur);
+    vm.viz.step();
+  }
+  let data = inputData[document.getElementById("selectInput").value].pop();
+  vm.invokeFunc(document.getElementById("selectMethod").value, undefined, data);
+};
