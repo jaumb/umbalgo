a4b39146041c20517982000a7ffc08951d811c67
Jason Curran <curran.jm@gmail.com>
2017-05-07 13:33:18 -0400

    updated about page

--- src/content/about.js
+++ src/content/about.js
@@ -2,24 +2,41 @@
 document.getElementById("container").innerHTML += `
 <div class="about-us">
   <div class="info">
-    
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+    <p align="left">
+      Algorithm Beacon provides visualizations of the algorithms described in
+      Robert Sedgewick and Kevin Wayne’s
+      <a href="http://algs4.cs.princeton.edu/home/"><i>Algorithms</i> (4th ed.)</a>.
+      The visualizations follow the java code provided in the book's
+      <a href="https://github.com/kevin-wayne/algs4">public repository</a>.
+      <i>Algorithms</i> has been an invaluable resource for us and we hope that
+      the visualizations on this site will serve as a useful complement to that
+      wonderful resource.
     </p>
 
-    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    <p align="left">
+      Algorithm Beacon is written primarily in javascript. The visualizations
+      are generated using a custom API that relies on the
+      <a href="https://d3js.org/">D3 (Data-Driven Documents)</a> library.
+      The site also makes use of of the
+      <a href="https://getbootstrap.com/">Bootstrap</a> framework.
+    </p>
+
+    <p align="left">
+      Algorithm Beacon was initially developed under the direction of Professor
+      Swami Iyer at the University of Massachusetts Boston for the MS capstone.
+    </p>
+
+    <p align="left">
+      Jeff Amari<br>
+      Jason Curran<br>
+      Kinjalk Parmar<br>
+      Nikhil Sidhwani<br>
+      Tory Wheelwright<br>
     </p>
   </div>
 
+  <!--
   <div class="row">
 
     <div class = "col-sm-6 col-md-2 col-md-offset-1">
@@ -83,6 +100,7 @@ document.getElementById("container").innerHTML += `
     </div>
 
   </div>
-  
+  -->
+
 </div>
 `;
